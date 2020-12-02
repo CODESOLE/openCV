6ac1a1fcb13c2f1d195a96c9be3cb88216ff3135
CODESOLE <canerselcuk4444@gmail.com>
2020-12-02 22:34:32 +0300

    test cases added

--- ocv.cc
+++ ocv.cc
@@ -4,12 +4,20 @@
 #include <opencv2/highgui.hpp>
 #include <opencv4/opencv2/opencv.hpp>
 
+enum class case_id
+{
+  READ_IMAGE = 0,
+  READ_VIDEO,
+  READ_WEBCAM,
+  HISTOGRAM_EQUALIZATION
+};
+
 void
-demos (size_t demo_id = 0)
+demos (case_id demo_id = case_id::READ_IMAGE)
 {
   switch (demo_id)
     {
-    case 0:
+    case case_id::READ_IMAGE:
       {
         // Read the image file
         cv::Mat image = cv::imread ("/home/caner/Pictures/boostNote.png");
@@ -19,22 +27,36 @@ demos (size_t demo_id = 0)
           {
             std::cout << "Could not open or find the image" << std::endl;
             std::cin.get (); // wait for any key press
-            return exit(EXIT_SUCCESS);
+            return exit (EXIT_SUCCESS);
           }
 
-        cv::String windowName = "OpenCV"; // Name of the window
+        // Create trackbar to change brightness
 
-        cv::namedWindow (windowName, cv::WINDOW_NORMAL); // Create a window
+        cv::namedWindow ("OpenCV", cv::WINDOW_AUTOSIZE); // Create a window
+        int iSliderValue1 = 50;
+        cv::createTrackbar ("Brightness", "OpenCV", &iSliderValue1, 100);
 
-        cv::imshow (windowName, image); // Show our image inside the created window.
+        while (true)
+          {
+            cv::Mat dst;
+            int iBrightness = iSliderValue1 - 50;
+            image.convertTo (dst, -1, 1, iBrightness);
 
-        cv::waitKey (0); // Wait for any keystroke in the window
+            // show the brightness and contrast adjusted image
+            cv::imshow ("OpenCV", dst);
 
-        cv::destroyWindow (windowName); // destroy the created window
+            // if user press 'ESC' key
+            if (cv::waitKey (50) == 27)
+              {
+                break;
+              }
+          }
+
+        cv::destroyWindow ("OpenCV"); // destroy the created window
 
         break;
       }
-    case 1:
+    case case_id::READ_VIDEO:
       {
         // open the video file for reading
         cv::VideoCapture cap ("/home/caner/Misc/non_lin/nonlinear_week2.mp4");
@@ -44,7 +66,7 @@ demos (size_t demo_id = 0)
           {
             std::cout << "Cannot open the video file" << std::endl;
             std::cin.get (); // wait for any key press
-            return exit(EXIT_FAILURE);
+            return exit (EXIT_FAILURE);
           }
 
         // Uncomment the following line if you want to start the video in the
@@ -80,22 +102,114 @@ demos (size_t demo_id = 0)
             if (cv::waitKey (10) == 27)
               {
                 std::cout << "Esc key is pressed by user. Stoppig the video"
-                     << std::endl;
+                          << std::endl;
                 break;
               }
           }
         break;
       }
-    case 2:{
+    case case_id::READ_WEBCAM:
+      {
+        // open the video file for reading
+        cv::VideoCapture cap (0);
 
-             break;
-           }
+        // if not success, exit program
+        if (cap.isOpened () == false)
+          {
+            std::cout << "Cannot open the video file" << std::endl;
+            std::cin.get (); // wait for any key press
+            return exit (EXIT_FAILURE);
+          }
+
+        double dWidth = cap.get (
+            cv::CAP_PROP_FRAME_WIDTH); // get the width of frames of the video
+        double dHeight
+            = cap.get (cv::CAP_PROP_FRAME_HEIGHT); // get the height of frames
+                                                   // of the video
+
+        std::cout << "Resolution of the video : " << dWidth << " x " << dHeight
+                  << std::endl;
+
+        // Uncomment the following line if you want to start the video in the
+        // middle cap.set(CAP_PROP_POS_MSEC, 300);
+
+        // get the frames rate of the video
+        double fps = cap.get (cv::CAP_PROP_FPS);
+        std::cout << "Frames per seconds : " << fps << std::endl;
+
+        cv::String window_name = "My First Video";
+
+        cv::namedWindow (window_name, cv::WINDOW_NORMAL); // create a window
+
+        while (true)
+          {
+            cv::Mat frame;
+            bool bSuccess = cap.read (frame); // read a new frame from video
+
+            // Breaking the while loop at the end of the video
+            if (bSuccess == false)
+              {
+                std::cout << "Found the end of the video" << std::endl;
+                break;
+              }
+
+            // show the frame in the created window
+            cv::imshow (window_name, frame);
+
+            // wait for for 10 ms until any key is pressed.
+            // If the 'Esc' key is pressed, break the while loop.
+            // If the any other key is pressed, continue the loop
+            // If any key is not pressed withing 10 ms, continue the loop
+            if (cv::waitKey (10) == 27)
+              {
+                std::cout << "Esc key is pressed by user. Stoppig the video"
+                          << std::endl;
+                break;
+              }
+          }
+        break;
+      }
+    case case_id::HISTOGRAM_EQUALIZATION:
+      {
+        // Read the image file
+        cv::Mat image = cv::imread ("/home/caner/Pictures/stockByte.png");
+
+        // Check for failure
+        if (image.empty ())
+          {
+            std::cout << "Could not open or find the image" << std::endl;
+            std::cin.get (); // wait for any key press
+            return exit (EXIT_SUCCESS);
+          }
+
+        // change the color image to grayscale image
+        cvtColor (image, image, cv::COLOR_BGR2GRAY);
+
+        // equalize the histogram
+        cv::Mat hist_equalized_image;
+        equalizeHist (image, hist_equalized_image);
+
+        cv::String windowName = "AFTER"; // Name of the window
+
+        cv::namedWindow (windowName, cv::WINDOW_NORMAL); // Create a window
+        cv::namedWindow ("BEFORE", cv::WINDOW_NORMAL);
+
+        cv::imshow (
+            windowName,
+            hist_equalized_image); // Show our image inside the created window.
+        cv::imshow ("BEFORE", image);
+
+        cv::waitKey (0); // Wait for any keystroke in the window
+
+        cv::destroyWindow (windowName); // destroy the created window
+        cv::destroyWindow ("AFTER");    // destroy the created window
+      }
     }
 }
 
 int
 main (int argc, char **argv)
 {
-  demos(1);
+  demos (case_id::READ_IMAGE);
   return 0;
 }
