bca141fde12d6d6a23644edbb95785df8f87966d
CODESOLE <canerselcuk4444@gmail.com>
2020-12-07 14:41:40 +0300

    image detection added

--- ocv.cc
+++ ocv.cc
@@ -1,7 +1,10 @@
 #include <cstdlib>
 #include <iostream>
+#include <opencv2/core.hpp>
 #include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv4/opencv2/opencv.hpp>
 
 enum class case_id
@@ -9,7 +12,8 @@ enum class case_id
   READ_IMAGE = 0,
   READ_VIDEO,
   READ_WEBCAM,
-  HISTOGRAM_EQUALIZATION
+  HISTOGRAM_EQUALIZATION,
+  OBJECT_DETECTION
 };
 
 void
@@ -17,6 +21,90 @@ demos (case_id demo_id = case_id::READ_IMAGE)
 {
   switch (demo_id)
     {
+    case case_id::OBJECT_DETECTION:
+      {
+        cv::Mat image = cv::imread ("/home/caner/Pictures/stockByte.png");
+
+        // Check for failure
+        if (image.empty ())
+          {
+            std::cout << "Could not open or find the image" << std::endl;
+            std::cin.get (); // wait for any key press
+            return exit (EXIT_SUCCESS);
+          }
+
+        cv::namedWindow (
+            "Control",
+            cv::WINDOW_AUTOSIZE); // create a window called "Control"
+
+        int iLowH = 0;
+        int iHighH = 179;
+
+        int iLowS = 0;
+        int iHighS = 255;
+
+        int iLowV = 0;
+        int iHighV = 255;
+
+        // Create trackbars in "Control" window
+        cv::createTrackbar ("LowH", "Control", &iLowH, 179); // Hue (0 - 179)
+        cv::createTrackbar ("HighH", "Control", &iHighH, 179);
+
+        cv::createTrackbar ("LowS", "Control", &iLowS,
+                            255); // Saturation (0 - 255)
+        cv::createTrackbar ("HighS", "Control", &iHighS, 255);
+
+        cv::createTrackbar ("LowV", "Control", &iLowV, 255); // Value (0 - 255)
+        cv::createTrackbar ("HighV", "Control", &iHighV, 255);
+
+        while (true)
+          {
+            cv::Mat imgHSV;
+
+            cv::cvtColor (image, imgHSV,
+                          cv::COLOR_BGR2HSV); // Convert the captured frame
+                                              // from BGR to HSV
+
+            cv::Mat imgThresholded;
+
+            cv::inRange (imgHSV, cv::Scalar (iLowH, iLowS, iLowV),
+                         cv::Scalar (iHighH, iHighS, iHighV),
+                         imgThresholded); // Threshold the image
+
+            // morphological opening (remove small objects from the foreground)
+            cv::erode (imgThresholded, imgThresholded,
+                       cv::getStructuringElement (cv::MORPH_ELLIPSE,
+                                                  cv::Size (5, 5)));
+
+            cv::dilate (imgThresholded, imgThresholded,
+                        cv::getStructuringElement (cv::MORPH_ELLIPSE,
+                                                   cv::Size (5, 5)));
+
+            // morphological closing (fill small holes in the foreground)
+            cv::dilate (imgThresholded, imgThresholded,
+                        cv::getStructuringElement (cv::MORPH_ELLIPSE,
+                                                   cv::Size (5, 5)));
+
+            cv::erode (imgThresholded, imgThresholded,
+                       cv::getStructuringElement (cv::MORPH_ELLIPSE,
+                                                  cv::Size (5, 5)));
+
+            cv::imshow ("Thresholded Image",
+                        imgThresholded);          // show the thresholded image
+            cv::imshow ("Original", image); // show the original image
+
+            if (cv::waitKey (30) == 27) // wait for 'esc' key press for 30ms.
+                                        // If 'esc' key is pressed, break loop
+              {
+                std::cout << "esc key is pressed by user" << std::endl;
+                break;
+              }
+          }
+        cv::destroyWindow ("Control"); // destroy the created window
+        cv::destroyWindow ("Original"); // destroy the created window
+        cv::destroyWindow ("Thresholded Image"); // destroy the created window
+        break;
+      }
     case case_id::READ_IMAGE:
       {
         // Read the image file
@@ -210,6 +298,6 @@ demos (case_id demo_id = case_id::READ_IMAGE)
 int
 main (int argc, char **argv)
 {
-  demos (case_id::READ_IMAGE);
+  demos (case_id::OBJECT_DETECTION);
   return 0;
 }
