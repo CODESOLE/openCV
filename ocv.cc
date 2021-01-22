6919da33fc6927436595595b7a3d1075de7c533c
CODESOLE <canerselcuk4444@gmail.com>
2021-01-22 17:29:32 +0300

    minor changes

--- ocv.cc
+++ ocv.cc
@@ -8,21 +8,18 @@
 #include <opencv4/opencv2/opencv.hpp>
 
 void
-demos ()
+run ()
 {
         cv::Mat image = cv::imread ("/home/caner/Pictures/nature2.png");
 
-        // Check for failure
         if (image.empty ())
           {
             std::cout << "Could not open or find the image" << std::endl;
-            std::cin.get (); // wait for any key press
+            std::cin.get (); 
             return exit (EXIT_SUCCESS);
           }
 
-        cv::namedWindow (
-            "Control",
-            cv::WINDOW_AUTOSIZE); // create a window called "Control"
+        cv::namedWindow ("Control", cv::WINDOW_AUTOSIZE);
 
         int iLowH = 0;
         int iHighH = 179;
@@ -33,68 +30,50 @@ demos ()
         int iLowV = 0;
         int iHighV = 255;
 
-        // Create trackbars in "Control" window
-        cv::createTrackbar ("LowH", "Control", &iLowH, 179); // Hue (0 - 179)
+        cv::createTrackbar ("LowH", "Control", &iLowH, 179); 
         cv::createTrackbar ("HighH", "Control", &iHighH, 179);
 
-        cv::createTrackbar ("LowS", "Control", &iLowS,
-                            255); // Saturation (0 - 255)
+        cv::createTrackbar ("LowS", "Control", &iLowS, 255);
         cv::createTrackbar ("HighS", "Control", &iHighS, 255);
 
-        cv::createTrackbar ("LowV", "Control", &iLowV, 255); // Value (0 - 255)
+        cv::createTrackbar ("LowV", "Control", &iLowV, 255);
         cv::createTrackbar ("HighV", "Control", &iHighV, 255);
 
         while (true)
           {
             cv::Mat imgHSV;
 
-            cv::cvtColor (image, imgHSV,
-                          cv::COLOR_BGR2HSV); // Convert the captured frame
-                                              // from BGR to HSV
+            cv::cvtColor (image, imgHSV, cv::COLOR_BGR2HSV); 
 
             cv::Mat imgThresholded;
 
-            cv::inRange (imgHSV, cv::Scalar (iLowH, iLowS, iLowV),
-                         cv::Scalar (iHighH, iHighS, iHighV),
-                         imgThresholded); // Threshold the image
+            cv::inRange (imgHSV, cv::Scalar (iLowH, iLowS, iLowV), cv::Scalar (iHighH, iHighS, iHighV), imgThresholded);
 
-            // morphological opening (remove small objects from the foreground)
-            cv::erode (imgThresholded, imgThresholded,
-                       cv::getStructuringElement (cv::MORPH_ELLIPSE,
-                                                  cv::Size (5, 5)));
+            cv::erode (imgThresholded, imgThresholded, cv::getStructuringElement (cv::MORPH_ELLIPSE, cv::Size (5, 5)));
 
-            cv::dilate (imgThresholded, imgThresholded,
-                        cv::getStructuringElement (cv::MORPH_ELLIPSE,
-                                                   cv::Size (5, 5)));
+            cv::dilate (imgThresholded, imgThresholded, cv::getStructuringElement (cv::MORPH_ELLIPSE, cv::Size (5, 5)));
 
-            // morphological closing (fill small holes in the foreground)
-            cv::dilate (imgThresholded, imgThresholded,
-                        cv::getStructuringElement (cv::MORPH_ELLIPSE,
-                                                   cv::Size (5, 5)));
+            cv::dilate (imgThresholded, imgThresholded, cv::getStructuringElement (cv::MORPH_ELLIPSE, cv::Size (5, 5)));
 
-            cv::erode (imgThresholded, imgThresholded,
-                       cv::getStructuringElement (cv::MORPH_ELLIPSE,
-                                                  cv::Size (5, 5)));
+            cv::erode (imgThresholded, imgThresholded, cv::getStructuringElement (cv::MORPH_ELLIPSE, cv::Size (5, 5)));
 
-            cv::imshow ("Thresholded Image",
-                        imgThresholded);          // show the thresholded image
-            cv::imshow ("Original", image); // show the original image
+            cv::imshow ("Thresholded Image", imgThresholded);
+            cv::imshow ("Original", image);
 
-            if (cv::waitKey (30) == 27) // wait for 'esc' key press for 30ms.
-                                        // If 'esc' key is pressed, break loop
+            if (cv::waitKey (30) == 27)
               {
                 std::cout << "esc key is pressed by user" << std::endl;
                 break;
               }
           }
-        cv::destroyWindow ("Control"); // destroy the created window
-        cv::destroyWindow ("Original"); // destroy the created window
-        cv::destroyWindow ("Thresholded Image"); // destroy the created window
+        cv::destroyWindow ("Control"); 
+        cv::destroyWindow ("Original");
+        cv::destroyWindow ("Thresholded Image");
 }
 
 int
 main (int argc, char **argv)
 {
-  demos ();
+  run ();
   return 0;
 }
