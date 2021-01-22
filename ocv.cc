cada95a1f9325538195ee65ba0c7c6531e83922f
CODESOLE <canerselcuk4444@gmail.com>
2021-01-22 16:54:06 +0300

    tarckbar added

--- ocv.cc
+++ ocv.cc
@@ -1,173 +1,303 @@
+#include <cstdlib>
 #include <iostream>
-#include <iterator>
 #include <opencv2/core.hpp>
-#include <opencv2/core/base.hpp>
 #include <opencv2/core/mat.hpp>
-#include <opencv2/core/matx.hpp>
 #include <opencv2/core/types.hpp>
-#include <opencv2/flann/logger.h>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
 #include <opencv4/opencv2/opencv.hpp>
 
-void run()
+enum class case_id
 {
-    cv::Mat image_reference = cv::imread("reference_image.png");
-    cv::Mat image_changed = cv::imread("reference_image.png");
+  READ_IMAGE = 0,
+  READ_VIDEO,
+  READ_WEBCAM,
+  HISTOGRAM_EQUALIZATION,
+  OBJECT_DETECTION
+};
 
-    if (image_reference.empty())
+void
+demos (case_id demo_id = case_id::READ_IMAGE)
+{
+  switch (demo_id)
     {
-        std::cout << "Could not open or find the image" << std::endl;
-        std::cin.get(); 
-        return exit(EXIT_SUCCESS);
+    case case_id::OBJECT_DETECTION:
+      {
+        cv::Mat image = cv::imread ("/home/caner/Pictures/stockByte.png");
+
+        // Check for failure
+        if (image.empty ())
+          {
+            std::cout << "Could not open or find the image" << std::endl;
+            std::cin.get (); // wait for any key press
+            return exit (EXIT_SUCCESS);
+          }
+
+        cv::namedWindow (
+            "Control",
+            cv::WINDOW_AUTOSIZE); // create a window called "Control"
+
+        int iLowH = 0;
+        int iHighH = 179;
+
+        int iLowS = 0;
+        int iHighS = 255;
+
+        int iLowV = 0;
+        int iHighV = 255;
+
+        // Create trackbars in "Control" window
+        cv::createTrackbar ("LowH", "Control", &iLowH, 179); // Hue (0 - 179)
+        cv::createTrackbar ("HighH", "Control", &iHighH, 179);
+
+        cv::createTrackbar ("LowS", "Control", &iLowS,
+                            255); // Saturation (0 - 255)
+        cv::createTrackbar ("HighS", "Control", &iHighS, 255);
+
+        cv::createTrackbar ("LowV", "Control", &iLowV, 255); // Value (0 - 255)
+        cv::createTrackbar ("HighV", "Control", &iHighV, 255);
+
+        while (true)
+          {
+            cv::Mat imgHSV;
+
+            cv::cvtColor (image, imgHSV,
+                          cv::COLOR_BGR2HSV); // Convert the captured frame
+                                              // from BGR to HSV
+
+            cv::Mat imgThresholded;
+
+            cv::inRange (imgHSV, cv::Scalar (iLowH, iLowS, iLowV),
+                         cv::Scalar (iHighH, iHighS, iHighV),
+                         imgThresholded); // Threshold the image
+
+            // morphological opening (remove small objects from the foreground)
+            cv::erode (imgThresholded, imgThresholded,
+                       cv::getStructuringElement (cv::MORPH_ELLIPSE,
+                                                  cv::Size (5, 5)));
+
+            cv::dilate (imgThresholded, imgThresholded,
+                        cv::getStructuringElement (cv::MORPH_ELLIPSE,
+                                                   cv::Size (5, 5)));
+
+            // morphological closing (fill small holes in the foreground)
+            cv::dilate (imgThresholded, imgThresholded,
+                        cv::getStructuringElement (cv::MORPH_ELLIPSE,
+                                                   cv::Size (5, 5)));
+
+            cv::erode (imgThresholded, imgThresholded,
+                       cv::getStructuringElement (cv::MORPH_ELLIPSE,
+                                                  cv::Size (5, 5)));
+
+            cv::imshow ("Thresholded Image",
+                        imgThresholded);          // show the thresholded image
+            cv::imshow ("Original", image); // show the original image
+
+            if (cv::waitKey (30) == 27) // wait for 'esc' key press for 30ms.
+                                        // If 'esc' key is pressed, break loop
+              {
+                std::cout << "esc key is pressed by user" << std::endl;
+                break;
+              }
+          }
+        cv::destroyWindow ("Control"); // destroy the created window
+        cv::destroyWindow ("Original"); // destroy the created window
+        cv::destroyWindow ("Thresholded Image"); // destroy the created window
+        break;
+      }
+    case case_id::READ_IMAGE:
+      {
+        // Read the image file
+        cv::Mat image = cv::imread ("/home/caner/Pictures/boostNote.png");
+
+        // Check for failure
+        if (image.empty ())
+          {
+            std::cout << "Could not open or find the image" << std::endl;
+            std::cin.get (); // wait for any key press
+            return exit (EXIT_SUCCESS);
+          }
+
+        // Create trackbar to change brightness
+
+        cv::namedWindow ("OpenCV", cv::WINDOW_AUTOSIZE); // Create a window
+        int iSliderValue1 = 50;
+        cv::createTrackbar ("Brightness", "OpenCV", &iSliderValue1, 100);
+
+        while (true)
+          {
+            cv::Mat dst;
+            int iBrightness = iSliderValue1 - 50;
+            image.convertTo (dst, -1, 1, iBrightness);
+
+            // show the brightness and contrast adjusted image
+            cv::imshow ("OpenCV", dst);
+
+            // if user press 'ESC' key
+            if (cv::waitKey (50) == 27)
+              {
+                break;
+              }
+          }
+
+        cv::destroyWindow ("OpenCV"); // destroy the created window
+
+        break;
+      }
+    case case_id::READ_VIDEO:
+      {
+        // open the video file for reading
+        cv::VideoCapture cap ("/home/caner/Misc/non_lin/nonlinear_week2.mp4");
+
+        // if not success, exit program
+        if (cap.isOpened () == false)
+          {
+            std::cout << "Cannot open the video file" << std::endl;
+            std::cin.get (); // wait for any key press
+            return exit (EXIT_FAILURE);
+          }
+
+        // Uncomment the following line if you want to start the video in the
+        // middle cap.set(CAP_PROP_POS_MSEC, 300);
+
+        // get the frames rate of the video
+        double fps = cap.get (cv::CAP_PROP_FPS);
+        std::cout << "Frames per seconds : " << fps << std::endl;
+
+        cv::String window_name = "My First Video";
+
+        cv::namedWindow (window_name, cv::WINDOW_NORMAL); // create a window
+
+        while (true)
+          {
+            cv::Mat frame;
+            bool bSuccess = cap.read (frame); // read a new frame from video
+
+            // Breaking the while loop at the end of the video
+            if (bSuccess == false)
+              {
+                std::cout << "Found the end of the video" << std::endl;
+                break;
+              }
+
+            // show the frame in the created window
+            cv::imshow (window_name, frame);
+
+            // wait for for 10 ms until any key is pressed.
+            // If the 'Esc' key is pressed, break the while loop.
+            // If the any other key is pressed, continue the loop
+            // If any key is not pressed withing 10 ms, continue the loop
+            if (cv::waitKey (10) == 27)
+              {
+                std::cout << "Esc key is pressed by user. Stoppig the video"
+                          << std::endl;
+                break;
+              }
+          }
+        break;
+      }
+    case case_id::READ_WEBCAM:
+      {
+        // open the video file for reading
+        cv::VideoCapture cap (0);
+
+        // if not success, exit program
+        if (cap.isOpened () == false)
+          {
+            std::cout << "Cannot open the video file" << std::endl;
+            std::cin.get (); // wait for any key press
+            return exit (EXIT_FAILURE);
+          }
+
+        double dWidth = cap.get (
+            cv::CAP_PROP_FRAME_WIDTH); // get the width of frames of the video
+        double dHeight
+            = cap.get (cv::CAP_PROP_FRAME_HEIGHT); // get the height of frames
+                                                   // of the video
+
+        std::cout << "Resolution of the video : " << dWidth << " x " << dHeight
+                  << std::endl;
+
+        // Uncomment the following line if you want to start the video in the
+        // middle cap.set(CAP_PROP_POS_MSEC, 300);
+
+        // get the frames rate of the video
+        double fps = cap.get (cv::CAP_PROP_FPS);
+        std::cout << "Frames per seconds : " << fps << std::endl;
+
+        cv::String window_name = "My First Video";
+
+        cv::namedWindow (window_name, cv::WINDOW_NORMAL); // create a window
+
+        while (true)
+          {
+            cv::Mat frame;
+            bool bSuccess = cap.read (frame); // read a new frame from video
+
+            // Breaking the while loop at the end of the video
+            if (bSuccess == false)
+              {
+                std::cout << "Found the end of the video" << std::endl;
+                break;
+              }
+
+            // show the frame in the created window
+            cv::imshow (window_name, frame);
+
+            // wait for for 10 ms until any key is pressed.
+            // If the 'Esc' key is pressed, break the while loop.
+            // If the any other key is pressed, continue the loop
+            // If any key is not pressed withing 10 ms, continue the loop
+            if (cv::waitKey (10) == 27)
+              {
+                std::cout << "Esc key is pressed by user. Stoppig the video"
+                          << std::endl;
+                break;
+              }
+          }
+        break;
+      }
+    case case_id::HISTOGRAM_EQUALIZATION:
+      {
+        // Read the image file
+        cv::Mat image = cv::imread ("/home/caner/Pictures/stockByte.png");
+
+        // Check for failure
+        if (image.empty ())
+          {
+            std::cout << "Could not open or find the image" << std::endl;
+            std::cin.get (); // wait for any key press
+            return exit (EXIT_SUCCESS);
+          }
+
+        // change the color image to grayscale image
+        cvtColor (image, image, cv::COLOR_BGR2GRAY);
+
+        // equalize the histogram
+        cv::Mat hist_equalized_image;
+        equalizeHist (image, hist_equalized_image);
+
+        cv::String windowName = "AFTER"; // Name of the window
+
+        cv::namedWindow (windowName, cv::WINDOW_NORMAL); // Create a window
+        cv::namedWindow ("BEFORE", cv::WINDOW_NORMAL);
+
+        cv::imshow (
+            windowName,
+            hist_equalized_image); // Show our image inside the created window.
+        cv::imshow ("BEFORE", image);
+
+        cv::waitKey (0); // Wait for any keystroke in the window
+
+        cv::destroyWindow (windowName); // destroy the created window
+        cv::destroyWindow ("AFTER");    // destroy the created window
+      }
     }
-
-    if (image_changed.empty())
-    {
-        std::cout << "Could not open or find the image" << std::endl;
-        std::cin.get(); 
-        return exit(EXIT_SUCCESS);
-    }
-
-    cv::namedWindow("Thresholded Image Reference", cv::WINDOW_NORMAL); 
-    cv::namedWindow("Thresholded Image Changed", cv::WINDOW_NORMAL); 
-
-    cv::namedWindow("Warning Window", cv::WINDOW_NORMAL);
-
-    const int iLowH = 11;
-    const int iHighH = 33;
-
-    const int iLowS = 14;
-    const int iHighS = 255;
-
-    const int iLowV = 4;
-    const int iHighV = 255;
-#if 0
-    // Create trackbars in "Control" window
-    cv::createTrackbar("LowH", "Control", &iLowH, 179); // Hue (0 - 179)
-    cv::createTrackbar("HighH", "Control", &iHighH, 179);
-
-    cv::createTrackbar("LowS", "Control", &iLowS, 255); // Saturation (0 - 255)
-    cv::createTrackbar("HighS", "Control", &iHighS, 255);
-
-    cv::createTrackbar("LowV", "Control", &iLowV, 255); // Value (0 - 255)
-    cv::createTrackbar("HighV", "Control", &iHighV, 255);
-#endif
-    while (true)
-    {
-        switch (std::getchar())
-        {
-        case '1':
-            image_changed = cv::imread("warning_1.png");
-            break;
-        case '2':
-            image_changed = cv::imread("warning_2.png");
-            break;
-        
-        case '3':
-            image_changed = cv::imread("no_warning_image.png");
-            break;
-        
-        default:
-            image_changed = cv::imread("reference_image.png");
-            break;
-        }
-
-        cv::Mat imgHSV_changed, imgHSV_reference;
-
-        cv::cvtColor(image_reference, imgHSV_reference, cv::COLOR_BGR2HSV);                                 
-        cv::cvtColor(image_changed, imgHSV_changed, cv::COLOR_BGR2HSV); 
-
-        cv::Mat imgThresholded_changed, imgThresholded_reference;
-
-        cv::inRange(imgHSV_reference, cv::Scalar(iLowH, iLowS, iLowV), cv::Scalar(iHighH, iHighS, iHighV), imgThresholded_reference); 
-
-        // morphological opening (remove small objects from the foreground)
-        cv::erode(imgThresholded_reference, imgThresholded_reference, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-        cv::dilate(imgThresholded_reference, imgThresholded_reference, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-
-        // morphological closing (fill small holes in the foreground)
-        cv::dilate(imgThresholded_reference, imgThresholded_reference, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-        cv::erode(imgThresholded_reference, imgThresholded_reference, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-        //----------------------------------------------------------------------------------------------------------------------------------------------
-        cv::inRange(imgHSV_changed, cv::Scalar(iLowH, iLowS, iLowV), cv::Scalar(iHighH, iHighS, iHighV), imgThresholded_changed); // Threshold the image
-
-        // morphological opening (remove small objects from the foreground)
-        cv::erode(imgThresholded_changed, imgThresholded_changed, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-        cv::dilate(imgThresholded_changed, imgThresholded_changed, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-
-        // morphological closing (fill small holes in the foreground)
-        cv::dilate(imgThresholded_changed, imgThresholded_changed, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-        cv::erode(imgThresholded_changed, imgThresholded_changed, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-        //----------------------------------------------------------------------------------------------------------------------------------------------
-        cv::imshow("Thresholded Image Changed", imgThresholded_changed);          
-        cv::imshow("Thresholded Image Reference", imgThresholded_reference); 
-
-        cv::Mat img_result = image_changed;
-        cv::cvtColor(img_result, img_result, cv::COLOR_BGR2GRAY); 
-
-        for (int i = 0; i < imgThresholded_changed.rows; i++)
-        {
-            for (int j = 0; j < imgThresholded_changed.cols; j++)
-            {
-                if ((int)imgThresholded_changed.at<uint8_t>(i, j) == (int)imgThresholded_reference.at<uint8_t>(i, j))
-                    img_result.at<uint8_t>(i, j) = 0;
-                else
-                    img_result.at<uint8_t>(i, j) = 255;
-            }
-        }
-        // ---------------------------------------------- WARNING WINDOW ---------------------------------------------------
-
-        bool isdifferent = false;
-
-        for (int i = 0; i < imgThresholded_changed.rows; i++)
-        {
-            for (int j = 0; j < imgThresholded_changed.cols; j++)
-            {
-                if ((int)imgThresholded_changed.at<uint8_t>(i, j) != (int)imgThresholded_reference.at<uint8_t>(i, j))
-                {
-                    isdifferent = true;
-                    goto warn_condtion;
-                }
-            }
-        }
-    warn_condtion:
-        if (isdifferent)
-        {
-            cv::Mat warn_text(320, 240, CV_8UC3, cv::Scalar(0, 0, 0));
-            cv::putText(warn_text, cv::String("WARNING!!!"), cv::Point2i(3,150), cv::FONT_HERSHEY_SIMPLEX, 1.4f, cv::Scalar(0, 0, 255));
-            cv::putText(warn_text, cv::String("OBJECT DETECTED"), cv::Point2i(30,200), cv::FONT_HERSHEY_SIMPLEX, .5f, cv::Scalar(0, 0, 255));
-            cv::putText(warn_text, cv::String("IN THE YELLOW LINE"), cv::Point2i(30,220), cv::FONT_HERSHEY_SIMPLEX, .5f, cv::Scalar(0, 0, 255));
-            cv::imshow("Warning Window", warn_text);
-            cv::imshow("Result Image", img_result);
-        }
-        else
-        {
-            cv::Mat warn_text(320, 240, CV_8UC3, cv::Scalar(0, 0, 0));
-            cv::putText(warn_text, cv::String("NO OBJECT DETECTED"), cv::Point2i(30, 110), cv::FONT_HERSHEY_SIMPLEX, .5f, cv::Scalar(0, 255, 0));
-            cv::putText(warn_text, cv::String("IN THE YELLOW LINE"), cv::Point2i(30, 130), cv::FONT_HERSHEY_SIMPLEX, .5f, cv::Scalar(0, 255, 0));
-            cv::imshow("Warning Window", warn_text);
-            cv::destroyWindow("Result Image");
-        }
-
-
-        if (cv::waitKey(30) == 27) 
-        {
-            std::cout << "esc key is pressed by user" << std::endl;
-            break;
-        }
-    }
-
-    cv::destroyWindow("Thresholded Image Reference"); 
-    cv::destroyWindow("Thresholded Image Changed");
-    cv::destroyWindow("Warning Window");
 }
 
-
-
-int main(int argc, char** argv)
+int
+main (int argc, char **argv)
 {
-    run();
-    return 0;
+  demos (case_id::OBJECT_DETECTION);
+  return 0;
 }
